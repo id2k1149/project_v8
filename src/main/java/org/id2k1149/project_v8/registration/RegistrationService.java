bb0db9bbba54f0b689b4eeffa916fac911613a10
id2k1149 <id2k1149@gmail.com>
2021-07-30 13:02:36 -0400

    v06 save registration

--- src/main/java/org/id2k1149/project_v8/registration/RegistrationService.java
+++ src/main/java/org/id2k1149/project_v8/registration/RegistrationService.java
@@ -1,10 +1,24 @@
 package org.id2k1149.project_v8.registration;
 
+import org.id2k1149.project_v8.models.User;
+import org.id2k1149.project_v8.models.UserRole;
+import org.id2k1149.project_v8.service.UserService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RegistrationService {
+
+    private final UserService userService;
+
+    public RegistrationService(UserService userService) {
+        this.userService = userService;
+    }
+
     public String register(RegistrationRequest request) {
-        return "it works";
+        return userService.addNewUser(new User(
+                request.getUsername(),
+                request.getPassword(),
+                UserRole.USER
+        ));
     }
 }
--- src/main/java/org/id2k1149/project_v8/security/config/WebSecurityConfig.java
+++ src/main/java/org/id2k1149/project_v8/security/config/WebSecurityConfig.java
@@ -28,13 +28,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
+
                 .authorizeRequests()
                   .antMatchers("api/v*/registration/**")
                   .permitAll()
-                .anyRequest()
-                    .authenticated()
-                    .and()
-                    .formLogin();
+
+//                .anyRequest()
+//                    .authenticated()
+//                    .and()
+//                    .formLogin()
+        ;
 
     }
 
