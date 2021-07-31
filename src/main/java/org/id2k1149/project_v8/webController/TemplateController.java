adfd799974be8a3f48e36243c0f0974e161db9ea
id2k1149 <id2k1149@gmail.com>
2021-07-30 21:55:17 -0400

    v13 login redirect to polls

---
+++ src/main/java/org/id2k1149/project_v8/webController/TemplateController.java
@@ -0,0 +1,20 @@
+package org.id2k1149.project_v8.webController;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class TemplateController {
+
+    @GetMapping("/login")
+    public String getLoginView() {
+        return "login";
+    }
+
+    @GetMapping("/polls")
+    public String getPolls() {
+        return "polls";
+    }
+}
