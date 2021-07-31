5fe8b6ae68137152fcb35bff3710f5b4d0283e03
id2k1149 <id2k1149@gmail.com>
2021-07-31 18:06:42 -0400

    v15 signUp

--- src/main/java/org/id2k1149/project_v8/security/WebSecurityConfig.java
+++ src/main/java/org/id2k1149/project_v8/security/WebSecurityConfig.java
@@ -1,26 +1,32 @@
 package org.id2k1149.project_v8.security;
 
 import org.id2k1149.project_v8.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final UserService userService;
     private final BCryptPasswordEncoder passwordEncoder;
+    private final UserService userService;
 
-    public WebSecurityConfig(UserService userService,
-                             BCryptPasswordEncoder passwordEncoder) {
-        this.userService = userService;
+    @Autowired
+    public WebSecurityConfig(BCryptPasswordEncoder passwordEncoder, UserService userService) {
         this.passwordEncoder = passwordEncoder;
+        this.userService = userService;
     }
 
     @Override
@@ -29,6 +35,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .csrf().disable()
 
                 .authorizeRequests()
+                  .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
+                  .antMatchers("/signup").permitAll()
                   .antMatchers("/api/v*/registration/**")
                   .permitAll()
 
@@ -36,6 +44,25 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                     .authenticated()
                     .and()
                     .formLogin()
+
+                .loginPage("/login")
+                    .permitAll()
+                    .defaultSuccessUrl("/polls", true)
+                    .passwordParameter("password")
+                    .usernameParameter("username")
+                .and()
+                    .rememberMe() // default 14 days
+                    .tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(21)) // change to 21 days
+                    .key("verysecuredkey")
+                    .rememberMeParameter("remember-me")
+                .and()
+                    .logout()
+                    .logoutUrl("/logout")
+                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET")) //need to delete when .csrf() is enabled
+                    .clearAuthentication(true)
+                    .invalidateHttpSession(true)
+                    .deleteCookies("JSESSIONID", "remember-me")
+                    .logoutSuccessUrl("/")
         ;
 
     }
@@ -51,6 +78,5 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         provider.setPasswordEncoder(passwordEncoder);
         provider.setUserDetailsService(userService);
         return provider;
-
     }
 }
---
+++ src/main/java/org/id2k1149/project_v8/webController/TemplateController.java
@@ -0,0 +1,71 @@
+package org.id2k1149.project_v8.webController;
+
+import org.id2k1149.project_v8.model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class TemplateController {
+//    private final UserService userService;
+//
+//    @Autowired
+//    public TemplateController(UserService userService) {
+//        this.userService = userService;
+//    }
+
+    @GetMapping("/login")
+    public String getLoginView() {
+        return "login";
+    }
+
+    @GetMapping("/polls")
+    public String getPolls() {
+        return "polls";
+    }
+
+    @GetMapping("/signup")
+    public String newUser(@ModelAttribute("newUser") User newUser) {
+        return "signup";
+    }
+
+//    @PostMapping()
+//    public String registration(@ModelAttribute("newUser") @Valid User newUser,
+//                               BindingResult bindingResult) {
+//
+//        if (bindingResult.hasErrors()) {
+//            return "registration";
+//        }
+//
+//        userService.addNewUser(
+//                new User(
+//                        newUser.getUsername(),
+//                        newUser.getPassword(),
+//                        Role.USER
+//                )
+//        );
+//        return "redirect:/login";
+//    }
+
+//    @PostMapping(
+//            consumes = MediaType.APPLICATION_JSON_VALUE,
+//            produces = MediaType.APPLICATION_JSON_VALUE)
+//    public ResponseEntity<User> create(@RequestBody User newUser) {
+//        User user = userService.addNewUser(newUser);
+//
+//        if (user == null) {
+//           throw new IllegalStateException();
+//        }
+//        return new ResponseEntity<>(user, HttpStatus.CREATED);
+//
+//    }
+
+//    @PostMapping("/registration")
+//    @ResponseStatus(value = HttpStatus.CREATED)
+//    public User createUser(@Valid @RequestBody User newUser) {
+//        return userRepository.save(newUser);
+//    }
+}
+
