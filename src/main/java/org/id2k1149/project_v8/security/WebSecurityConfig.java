e4e37d1dbfd6034c8ee99313d37deea3ff0c98c6
id2k1149 <id2k1149@gmail.com>
2021-07-31 18:40:07 -0400

    v16 front

--- src/main/java/org/id2k1149/project_v8/security/WebSecurityConfig.java
+++ src/main/java/org/id2k1149/project_v8/security/WebSecurityConfig.java
@@ -1,22 +1,29 @@
 package org.id2k1149.project_v8.security;
 
 import org.id2k1149.project_v8.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final UserService userService;
     private final BCryptPasswordEncoder passwordEncoder;
 
+    @Autowired
     public WebSecurityConfig(UserService userService,
                              BCryptPasswordEncoder passwordEncoder) {
         this.userService = userService;
@@ -32,27 +39,37 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/",
                 "index",
                 "/css/*",
-                "/js/*",
-                "/api/v*/registration/**")
+                "/js/*"
+                )
                 .permitAll()
+                .antMatchers("/signup").permitAll()
+                .antMatchers("/api/v*/registration/**").permitAll()
+
         .anyRequest()
         .authenticated()
         .and()
         .formLogin()
-        .loginPage("/login")
-                .permitAll()
-        .defaultSuccessUrl("/polls", true)
 
-//                .authorizeRequests()
-//                  .antMatchers("/api/v*/registration/**")
-//                  .permitAll()
-//
-//                .anyRequest()
-//                    .authenticated()
-//                    .and()
-//                    .formLogin()
+        .loginPage("/login").permitAll()
+            .defaultSuccessUrl("/polls", true)
+            .passwordParameter("password")
+            .usernameParameter("username")
+
+        .and()
+            .rememberMe() // default 14 days
+            .tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(21)) // change to 21 days
+            .key("verysecuredkey")
+            .rememberMeParameter("remember-me")
+
+        .and()
+        .logout()
+            .logoutUrl("/logout")
+            .logoutRequestMatcher(new AntPathRequestMatcher("/", "GET")) //need to delete when .csrf() is enabled
+            .clearAuthentication(true)
+            .invalidateHttpSession(true)
+            .deleteCookies("JSESSIONID", "remember-me")
+            .logoutSuccessUrl("/login")
         ;
-
     }
 
     @Override
